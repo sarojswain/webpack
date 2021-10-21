0b8b2311414a8fdace3bdd914992cbb4d70814d8
Saroj Swain <sarojkumarswain51@gmail.com>
2021-10-21 19:22:34 +0530

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader", // 3. Injects styles into DOM
+          "css-loader", // 2. Turns css into commonjs
+          "sass-loader", // 1. Turns sass into css
+        ],
+      },
+      {
+        test: /\.html/,
+        use: ["html-loader"],
+      },
+      // {
+      //   test: /\.(svg|png|jpg|gif)$/,
+      //   use: {
+      //     loader: "file-loader",
+      //     options: {
+      //       name: "[name].[hash].[ext]",
+      //       outputPath: "imgs",
+      //     },
+      //   },
+      // },
+    ],
+  },
+};
